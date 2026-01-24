bf1d636af4cd428cc995502bf0fcdbf3db67f021
Andy Palmero <4ndyraul@gmail.com>
2026-01-24 17:31:22 -0300

    fix: adjust book grid layout for improved responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -72,7 +72,7 @@ export function Home() {
         </div>
       ) : (
         /* Book grid */
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {books.map((book) => (
             <BookCard
               key={book.id}
