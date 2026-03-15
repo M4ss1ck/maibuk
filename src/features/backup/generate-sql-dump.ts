bb5f21d0101f5648ada594411139dd489977f79a
Andy Palmero <4ndyraul@gmail.com>
2026-03-15 15:32:51 -0300

    feat(backup): add generateSqlDump utility

---
+++ src/features/backup/generate-sql-dump.ts
@@ -0,0 +1,6 @@
+import { exportDatabase } from "../../lib/db";
+
+export async function generateSqlDump(): Promise<string> {
+  const data = await exportDatabase();
+  return new TextDecoder().decode(data);
+}
